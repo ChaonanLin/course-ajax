e84df6c1420c90d169811c8091a5124ebb081504
Savoir <savoir.cn@gmail.com>
2018-11-15 21:56:51 -0800

    use ajax with jQery to add image and ajax Fetch api to add articles

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,20 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID 67cb76aae7448eba4e939b3bcb169d31f01ae3033634e74d61e53891702b0c35'
+            }
+        }).done(addImage);
     });
+
+    function addImage(image) {
+        const firstImage = image.results[0];
+        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+        <img src="${firstImage.urls.small}" alt="${searchedForText}">
+        <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`
+        );
+    }
 })();
--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,23 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=a76f3318b4cb4d0ab84c439c220b7198`
+        ).then(response => response.json())
+            .then(addArticles);
     });
+
+    function addArticles(data) {
+        let htmlContent = '';
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                <p>${article.snippet}</p>
+                </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
 })();
